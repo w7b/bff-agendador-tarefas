3838b82e64609a7c4c5d1c328fe4eb14709a1e37
w7b <pessoal.gabriel21@gmail.com>
2025-02-04 09:42:43 -0300

    adicionado esqueleto do projeto

---
+++ src/test/java/com/smoothy/bff_agendador_tarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothy.bff_agendador_tarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
